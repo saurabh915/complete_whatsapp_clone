9870acf7d49482dded47de4fdc7b76352caf1a33
saurabh patil <saurabhspatil4545@gmail.com>
2024-05-02 13:23:20 +0530

    final

--- socket/index.js
+++ socket/index.js
@@ -10,16 +10,24 @@ const addUser = (userData,socketId)=>{
 
 
 }
+const getUser = (userId)=>{
+    return users.find(user=>user.sub == userId );
+}
 
 
 io.on("connection",(socket)=>{
 console.log("user connected");
 socket.on("addUsers",userData =>{
+    
     addUser(userData,socket.id);
+    console.log("users are");
+    console.log(users);
     io.emit("getUsers",users);
 })
 
 socket.on("sendMessage" ,data =>{
     const user = getUser(data.receiverId);
+    console.log("message sent from sender "+ user.socketId);
+    io.to(user.socketId).emit('getMessage',data);
 })
 })
